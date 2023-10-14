9613d847695c070447a90eec8e6ebdf4c4011bb1
Vasile-Marinel <vasilemarinel2136@gmail.com>
2023-10-14 20:38:15 +0300

    Update problema_reginelor.c

--- problema_reginelor.c
+++ problema_reginelor.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define N 8
+
+int tabla[N][N];
+int numarSolutii=0;
+
+//Functia pentru afisarea tablei de sah
+void afiseazaTabla()
+{
+    numarSolutii++;
+    printf("\nSolutia %d:\n", numarSolutii);
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            //Afiseaza liniile orizontale ale tablei de sah
+            printf("-----");
+        }
+       printf("\n");
+
+        for(int j = 0; j < N; j++)
+        {
+            //Afiseaza liniile verticale si piesele de sah
+            printf("|  ");
+            printf("%d ", tabla[i][j]);
+        }
+        printf("|\n");
+    }
+
+// Afișează ultima linie orizontală a tablei de șah
+    for(int j = 0; j < N; j++)
+    {
+        printf("-----");
+    }
+    printf("\n");
+}
+
+
 
 int main()
 {
-    printf("Hello World");
+    //Initializam tabla de sah cu 0
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            tabla[i][j]=0;
+    
+    afiseazaTabla();
     return 0;
 }
