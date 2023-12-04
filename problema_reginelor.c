31b8ff79b9751ac86705a5a3c8359fa10c3c5d33
Vasile-Marinel <vasilemarinel2136@gmail.com>
2023-12-04 16:15:13 +0200

    Update problema_reginelor.c

--- problema_reginelor.c
+++ problema_reginelor.c
@@ -39,21 +39,21 @@ void afiseazaTabla()
 }
 
 //Functia care verifica daca o regina poate fi plasata intr-o anumita pozitie
-bool esteSigur(int rand, int coloana)
+bool esteSigur(int linie, int coloana)
 {
     //Verificam randul pe stanga 
     for(int i = 0; i < coloana; i++)
-        if(tabla[rand][i])
+        if(tabla[linie][i] == 1)
             return false;
 
     //Verificam diagonala sus-stanga
-    for(int i = rand, j = coloana; i >= 0 && j >= 0; i--, j--)
-        if(tabla[i][j])
+    for(int i = linie, j = coloana; i >= 0 && j >= 0; i--, j--)
+        if(tabla[i][j] == 1)
             return false;
 
     // Verificam diagonala jos-stanga
-    for (int i = rand, j = coloana; i < N && j >= 0; i++, j--)
-        if (tabla[i][j])
+    for (int i = linie, j = coloana; i < N && j >= 0; i++, j--)
+        if (tabla[i][j] == 1)
             return false;
 
     return true;
@@ -71,7 +71,7 @@ bool rezolvaProblemaReginelor(int coloana)
     bool rezultat = false;
     for(int i = 0; i < N; i++)
     {
-        if(esteSigur(i, coloana))
+        if(esteSigur(i, coloana) == 1)
         {
             tabla[i][coloana]=1;
 
@@ -91,7 +91,7 @@ int main()
         for(int j = 0; j < N; j++)
             tabla[i][j]=0;
     
-    if(!rezolvaProblemaReginelor(0))
+    if(rezolvaProblemaReginelor(0) != 0)  //Daca nu exista solutie, afisam mesajul
     {
         printf("Nu exista solutie.");
     }
